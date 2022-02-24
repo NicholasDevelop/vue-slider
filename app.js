2aaa2e404eba80b3bf7704476c39acfee67f92fd
NicholasDevelop <marcaccini.n@gmail.com>
2022-02-24 14:09:41 +0100

    sistemato codice html e iniziato a modificare js

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-const slides = [
-    {
-        items : 'img/01.jpg',
-        title : 'Svezia',
-        text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+const app = new Vue ({
+    el: '#app',
+    data: {
+        slides: [
+            {
+                items : 'img/01.jpg',
+                title : 'Svezia',
+                text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                items : 'img/02.jpg',
+                title : 'Svizzera',
+                text : 'Lorem ipsum',
+            },
+            {
+                items : 'img/03.jpg',
+                title : 'Gran Bretagna',
+                text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                items : 'img/04.jpg',
+                title : 'Germania',
+                text :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                items : 'img/05.jpg',
+                title : 'Paradise',
+                text : 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            }
+        ],
+        
     },
-    {
-        items : 'img/02.jpg',
-        title : 'Svizzera',
-        text : 'Lorem ipsum',
-    },
-    {
-        items : 'img/03.jpg',
-        title : 'Gran Bretagna',
-        text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        items : 'img/04.jpg',
-        title : 'Germania',
-        text :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    },
-    {
-        items : 'img/05.jpg',
-        title : 'Paradise',
-        text : 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    methods: {
+
     }
-];
+})
+
+
 
 
 const slideWrapper = document.querySelector('.slide-wrapper');
